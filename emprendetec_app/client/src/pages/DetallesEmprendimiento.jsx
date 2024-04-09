37982c42d1129bc272a98a05112b1a96044d4807
jpbr66 <jburgos1000@gmail.com>
2024-04-09 06:53:59 -0600

    detalle

--- emprendetec_app/client/src/pages/DetallesEmprendimiento.jsx
+++ emprendetec_app/client/src/pages/DetallesEmprendimiento.jsx
@@ -17,10 +17,10 @@ export default function DetallesPost() {
         try {
             const response = await axios.get(`/api/emprendimientos/${params.id}`);
             
-            if (response.data && response.data.post) {
+            if (response.data && response.data.post && response.data.post.length > 0) {
                 console.log("Se obtuvo el emprendimeinto")
-                console.log(response.data.post.Title)
-                setPost(response.data.post);
+                console.log(response.data.post[0].Title)
+                setPost(response.data.post[0]);
                 //setX(1);
             }
         } catch (error) {
@@ -36,14 +36,13 @@ export default function DetallesPost() {
         </Helmet>
         <div className="flex min-h-screen w-full items-start justify-start bg-white p-32">
           <div>
-            <img src={post.ImagePost} alt="emprendimiento.jpg" />
+            <img src={post.ImagePost} alt="emprendimiento.jpg" className="w-96"/>
           </div>
           {/* EMPRENDIMIENTOS */}
           <div className="w-full ml-16 mr-24">
-            <h1 className="text-3xl font-bold mb-8">{params.id}</h1>
-            <h1 className="text-3xl font-bold mb-8">{post.Title}</h1>
-            <Rating value={post.Score} />
-            <p className="mt-8">{post.DescriptionPost}</p>
+            <h1 className="text-3xl font-bold mb-8">{post && post.Title}</h1>
+            <Rating value={post && post.Score} />
+            <p className="mt-8">{post && post.DescriptionPost}</p>
             {/* <div className="">
                 d
             </div> */}
